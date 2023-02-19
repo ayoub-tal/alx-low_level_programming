db98831395d01615067aebc562eb6470bc8e660d
ayoubtalousten <talousten@gmail.com>
2023-02-19 08:53:40 -0800

    100 print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,26 @@
  */
 int main(void)
 {
-	int i, j;
+	int c, i;
 
-	for (i = 0; i <= 8; i++)
+	c = i = '0';
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
+			putchar(c);
+			putchar(i);
+
+			if ((c != '9') || (c == '9' && i != '9'))
 			{
-				putchar('\n');
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
