03a35227224b8dfd1699a22d503e2e1616e1dcdf
ayoubtalousten <talousten@gmail.com>
2023-02-19 09:25:55 -0800

    100-print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,16 @@
  */
 int main(void)
 {
-	int c, i;
+	int i, j;
 
-	c = i = '0';
-
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i < 9; i++)
 	{
-		for (i = '0'; i <= '9'; i++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar(c);
-			putchar(i);
+			putchar(i + '0');
+			putchar(j + '0');
 
-			if ((c != '9') || (c == '9' && i != '9'))
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
