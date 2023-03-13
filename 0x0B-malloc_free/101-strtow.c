057049d144e9058d6a5e96fa6741df57c1844477
ayoubtalousten <talousten@gmail.com>
2023-03-13 11:53:29 -0700

    c is fun

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,7 +12,6 @@ char **strtow(char *str);
  *
  * Return: The index marking the end of the initial word pointed to by str.
  */
-
 int word_len(char *str)
 {
 	int index = 0, len = 0;
@@ -32,7 +31,6 @@ int word_len(char *str)
  *
  * Return: The number of words contained within str.
  */
-
 int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
@@ -59,31 +57,30 @@ int count_words(char *str)
  * Return: If str = NULL, str = "", or the function fails - NULL.
  *         Otherwise - a pointer to an array of strings (words).
  */
-
 char **strtow(char *str)
 {
 	char **strings;
-	int index = 0, word, w, letters, l;
+	int index = 0, words, w, letters, l;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	word = count_words(str);
-	if (word == 0)
+	words = count_words(str);
+	if (words == 0)
 		return (NULL);
 
-	strings = malloc(sizeof(char *) * (word + 1));
+	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
 
-	for (w = 0; w < word; w++)
+	for (w = 0; w < words; w++)
 	{
 		while (str[index] == ' ')
 			index++;
 
 		letters = word_len(str + index);
 
-		strings[w] = malloc(sizeof(char) * (letters = +1));
+		strings[w] = malloc(sizeof(char) * (letters + 1));
 
 		if (strings[w] == NULL)
 		{
